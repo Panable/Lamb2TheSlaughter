caaf0c352643dc8350ed046737c45158299df03e
Panable <directeddan@gmail.com>
2020-06-10 10:52:18 +1000

    hopefully this will fixit

--- Lamb-To-The-Slaughter/Assets/Scripts/PlayerStuff/WeaponSelect.cs
+++ Lamb-To-The-Slaughter/Assets/Scripts/PlayerStuff/WeaponSelect.cs
@@ -162,17 +162,7 @@ public class WeaponSelect : MonoBehaviour
         Inputs();
         AOEgraphicsReset();
 
-<<<<<<< HEAD
         selectedWeapon.Update();
-=======
-        anim.SetInteger("Reload", ammoCount);
-
-
-        if (ammoCount < 1)
-        {
-            ammoCount = 5;
-        }
->>>>>>> parent of b98af111... Check Description
 
         GravityBomb();
         ExplosiveBomb();
@@ -294,14 +284,11 @@ public class WeaponSelect : MonoBehaviour
         originalLD = AOElD.intensity.value;
         originalV = AOEv.color.value;
         Debug.Log(originalV);
-<<<<<<< HEAD
     }
 
     public void StartWeaponCoroutine(IEnumerator coroutineMethod)
     {
         StartCoroutine(coroutineMethod);
-=======
->>>>>>> parent of b98af111... Check Description
     }
 
 }
