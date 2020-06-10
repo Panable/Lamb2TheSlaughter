31fbb14f1d2b07572f47386fefb657e000b0e0c7
Ansaar13 <63997283+Ansaar13@users.noreply.github.com>
2020-06-10 14:29:56 +1000

    Blood Text Complete

---
+++ Lamb-To-The-Slaughter/Assets/Scripts/Procedural Generation/BloodMessages.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodMessages : MonoBehaviour
+{
+    public GameObject[] bMplane;
+    public Texture[] bMtexture;
+    Renderer bMplaneRenderer;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        bMplane = GameObject.FindGameObjectsWithTag("BloodMessage");
+        int[] usedTextures = new int[bMtexture.Length];
+        for (int i = 0; i < bMtexture.Length; i++)
+        {
+            usedTextures[i] = 0;
+        }
+        foreach (GameObject messageAnchor in bMplane)
+        {
+            int num = Random.Range(0, bMtexture.Length);
+
+            while (usedTextures[num] != 0)
+            {
+                num++;
+                if(num >= bMtexture.Length)
+                {
+                    num = 0;
+                }
+            }
+            usedTextures[num] = 1;
+
+            bMplaneRenderer = messageAnchor.GetComponent<Renderer>();
+            bMplaneRenderer.material.mainTexture = bMtexture[num];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
