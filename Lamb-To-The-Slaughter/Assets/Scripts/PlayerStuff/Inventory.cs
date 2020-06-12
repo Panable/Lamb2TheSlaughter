508247e28a229b8d27e837e429703518e798c426
Lachlan22 <63986090+lachlancooper22@users.noreply.github.com>
2020-06-12 22:56:50 +1000

    Inventory GUI Display
    - GUI updates to display inventory
    - Added TMPro variables.
    - Added number indicator under tools

---
+++ Lamb-To-The-Slaughter/Assets/Scripts/PlayerStuff/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour //Lachlan
+{
+    //Inventory Values
+    public int explosionBomb;
+    public int teleportBomb;
+    public int gravityBomb;
+    public int gasBomb;
+    public int medpack;
+    public int keys;
+
+    //Text Values
+    public TMP_Text explosionBombTXT;
+    public TMP_Text teleportBombTXT;
+    public TMP_Text gravityBombTXT;
+    public TMP_Text gasBombTXT;
+    public TMP_Text medpackTXT;
+    public TMP_Text keysTXT;
+
+    private void Update()
+    {
+        ToolsToText();
+    }
+
+    void ToolsToText()
+    {
+        explosionBombTXT.text = explosionBomb.ToString();
+        teleportBombTXT.text = teleportBomb.ToString();
+        gravityBombTXT.text = gravityBomb.ToString();
+        gasBombTXT.text = gasBomb.ToString();
+        medpackTXT.text = medpack.ToString();
+        keysTXT.text = keys.ToString();
+    }
+}
