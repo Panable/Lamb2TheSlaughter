42ce367695248ab5a936906301b23f9a4c31aee9
Ansaar13 <63997283+Ansaar13@users.noreply.github.com>
2020-06-21 13:57:53 +1000

    Resposdine Dev

---
+++ Lamb-To-The-Slaughter/Assets/Scripts/Health Scripts/ResHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResHealth : Health
+{
+    public float health;
+    public bool isDead = false;
+    public ParticleSystem hurtParticles;
+    Vector3 particleLocation;
+    public CapsuleCollider mainCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        particleLocation = transform.TransformPoint(mainCollider.center);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = base.currentHealth;
+
+        if (health < 1)
+        {
+            isDead = true;
+        }
+    }
+
+    public override void TakeDamage(float amount)
+    {
+        base.TakeDamage(amount);
+
+        Instantiate(hurtParticles, particleLocation, transform.localRotation);
+    }
+}
