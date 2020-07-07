a58e940eb629947d0a7bed8eed18273fa953c631
Ansaar13 <63997283+Ansaar13@users.noreply.github.com>
2020-07-07 20:32:10 +1000

    For Dhaddy

--- Lamb-To-The-Slaughter/Assets/Scripts/Menus/MainMenu.cs
+++ Lamb-To-The-Slaughter/Assets/Scripts/Menus/MainMenu.cs
@@ -45,7 +45,7 @@ public class MainMenu : MonoBehaviour //Lachlan
     {
         main.volume = 0.5f;
         main.PlayOneShot(startClip);
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 
     //When button is pressed the game will quit and print to the console, "Game Quit"
