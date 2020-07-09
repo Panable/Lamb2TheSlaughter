26e46b7358dec529581ab8019c71b0c805a7143d
Ansaar13 <63997283+Ansaar13@users.noreply.github.com>
2020-07-10 03:17:47 +1000

    Didnt wanna push the artifacts
    God damn i should be asleep

--- Lamb-To-The-Slaughter/Assets/Scripts/AI/BaseEnemy.cs
+++ Lamb-To-The-Slaughter/Assets/Scripts/AI/BaseEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public abstract class BaseEnemy : Component //NEEDS COMMENTING
+public abstract class BaseEnemy : Component
 {
     [HideInInspector]
     public GameObject obj;
